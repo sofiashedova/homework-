592826ca10b0e5ab502afc0ce78d4ce10a8c1435
mo1odoy4 <143870378+mo1odoy4@users.noreply.github.com>
2023-09-23 11:02:53 +0300

    dz 2

---
+++ dz/2.py
@@ -0,0 +1,9 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+f = float(input())
+if (f - d) == 0:
+    print("Делить на ноль нельзя!")
+else:
+    print((a * b - c) / (f - d))
